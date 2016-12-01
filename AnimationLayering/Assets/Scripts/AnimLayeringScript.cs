684d5c970a690bfa24b197a172f96ad0708a084c
Henslock <henslock123@gmail.com>
2016-11-30 19:05:36 -0500

    Completion Commit.

---
+++ AnimationLayering/Assets/Scripts/AnimLayeringScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class AnimLayeringScript : MonoBehaviour {
+    public Animator anim;
+    public Text textW;
+    private float x, y = 0.0f;
+    private float weight = 0.0f;
+
+	void Update () {
+        x = (Input.GetAxis("Horizontal"));
+        y = (Input.GetAxis("Vertical"));
+
+        anim.SetFloat("X", x);
+        anim.SetFloat("Y", y);
+
+        if (Input.GetButton("L1"))
+        {
+            weight -= 0.01f;
+            if (weight <= 0)
+                weight = 0.0f;
+        }
+
+        if (Input.GetButton("R1"))
+        {
+            weight += 0.01f;
+            if (weight >= 1)
+                weight = 1.0f;
+        }
+
+        textW.text = ("Animaton Weight : " + weight.ToString("F2"));
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetLayerWeight(1, weight);
+            anim.Play("attack_01", 1);
+        }
+    }
+}
---
+++ AnimationLayering/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+
+	void Update ()
+    {
+        float angH = Input.GetAxis("AnalogHorizontal") * 3;
+
+        if (angH >= 1.0f || angH <= -1.0f)
+        {
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + angH, 0);
+        }
+    }
+}
